946c6a055bc2cd7ad8cc9529b14b2855ee4b0594
shirakomamu <mamu@shirako.dev>
2021-05-17 19:53:53 -0700

    import workbox from local

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   pwa: {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: "public/service-worker.js",
+      importWorkboxFrom: "local",
     },
     name: "Shirako Mamu",
     themeColor: "#008aff",
