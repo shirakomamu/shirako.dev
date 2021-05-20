c75363c6c5548249d6fb70766486b6a4ecfd3cff
shirakomamu <mamu@shirako.dev>
2021-05-19 20:47:24 -0700

    home screen styling

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,13 @@
   <div class="nav h-12 flex items-center">
     <div class="justify-start ml-8 flex flex-grow items-center">
       <span class="text-gray-500 text-sm"
-        >© {{ new Date().getFullYear() }} 白子マム</span
+        >© {{ new Date().getFullYear() }} 白子マム |
+        <a
+          ref="emailLink"
+          :href="emailMailto"
+          class="email-link text-blue-500 hover:underline"
+          >{{ email }}</a
+        ></span
       >
     </div>
     <div class="justify-end space-x-8 mr-8 flex items-center">
@@ -12,15 +18,42 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 
 export default defineComponent({
   name: "AppFooter",
   setup() {
     const hash = process.env.VUE_APP_GIT_HASH || "n/a";
-    return { hash };
+
+    let emailMailto = ref("");
+    let email = ref("");
+    const emailLink = ref<HTMLElement | undefined>();
+
+    onMounted(() => {
+      setTimeout(() => {
+        const emailLinkRef = emailLink.value;
+
+        const name = "hello";
+        const domain = "shirako";
+        const tld = "dev";
+        const emailAddress = name + "@" + domain + "." + tld;
+        email.value = emailAddress;
+        emailMailto.value = "mailto" + ":" + emailAddress;
+
+        if (emailLinkRef) {
+          emailLinkRef.style.opacity = "1";
+        }
+      }, 3000);
+    });
+
+    return { hash, email, emailMailto, emailLink };
   },
 });
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.email-link {
+  opacity: 0;
+  transition: opacity 1s ease;
+}
+</style>
