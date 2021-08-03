624450d0b3af3f05fb48e53f2dbe33b786bf7d05
shirakomamu <mamu@shirako.dev>
2021-08-03 15:42:32 -0700

    add accordion, use it for projectitem

---
+++ src/components/Accordion.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="accordion-container">
+    <button
+      type="button"
+      class="accordion-title p-0 w-full text-left relative"
+      @click="toggleVisibility"
+      :alt="visible ? 'Hide accordion' : 'Show accordion'"
+    >
+      <div class="absolute top-0 left-0 h-full w-auto mr-2 text-2xl">
+        <ExpandLess v-if="!visible" class="icon-inline" /><ExpandMore
+          v-else
+          class="icon-inline"
+        />
+      </div>
+      <div class="ml-8 inline-block">
+        <slot name="title" />
+      </div>
+    </button>
+    <div
+      ref="contentsContainer"
+      class="accordion-contents overflow-y-hidden"
+      :class="{
+        'accordion-active': visible,
+      }"
+      :style="{
+        '--r-h': realHeight,
+      }"
+    >
+      <!-- https://stackoverflow.com/questions/1762539/margin-on-child-element-moves-parent-element -->
+      <div ref="contents" class="pt-1 -mt-1">
+        <slot name="default" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted, onUnmounted, ref } from "vue";
+import ExpandLess from "./icons/ExpandLess.vue";
+import ExpandMore from "./icons/ExpandMore.vue";
+
+export default defineComponent({
+  name: "Accordion",
+  components: {
+    ExpandLess,
+    ExpandMore,
+  },
+  props: {
+    initialVisibility: {
+      type: Boolean,
+      default: false,
+    },
+    showArrow: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup(props) {
+    const visible = ref<boolean>(props.initialVisibility);
+    const contentsContainer = ref<null | HTMLDivElement>(null);
+    const contents = ref<null | HTMLDivElement>(null);
+
+    const toggleVisibility = () => {
+      if (!contentsContainer.value) return;
+      contentsContainer.value.style.transition = "height 0.2s ease";
+      visible.value = !visible.value;
+      setTimeout(() => {
+        if (!contentsContainer.value) return;
+        contentsContainer.value.style.transition = "";
+      }, 200);
+    };
+    const realHeightInt = ref<number>(0);
+    const realHeight = computed(() => realHeightInt.value + "px");
+
+    const timer = ref<null | number>(0);
+
+    onMounted(() => {
+      setRealHeight();
+      timer.value = setInterval(() => {
+        setRealHeight();
+      }, 200);
+    });
+
+    onUnmounted(() => (timer.value = null));
+
+    const setRealHeight = () => {
+      const sh = contents.value?.clientHeight || 0;
+      realHeightInt.value = sh;
+    };
+
+    return {
+      visible,
+      toggleVisibility,
+      contentsContainer,
+      contents,
+      realHeight,
+      realHeightInt,
+      setRealHeight,
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.accordion-contents {
+  height: 0px;
+
+  &.accordion-active {
+    height: var(--r-h);
+  }
+}
+</style>
--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,45 +1,37 @@
 <template>
   <article class="grid grid-cols-1 gap-4">
-    <div>
-      <component
-        :is="url ? 'a' : 'div'"
-        class="text-2xl font-semibold"
-        :class="{
-          'hover:font-bold focus:font-bold': url,
-        }"
-        :href="url || undefined"
-        :target="url ? '_blank' : undefined"
-        :rel="url ? 'noopener noreferrer' : undefined"
-      >
-        <img
-          v-if="iconUrl"
-          :src="iconUrl"
-          width="32"
-          height="32"
-          :alt="`${title} logo`"
-          class="icon-inline mr-2 rounded-lg"
-        />{{ title }}
-        <OpenInNew v-if="url" class="icon-inline opacity-50 text-lg" />
-      </component>
-    </div>
-    <p v-if="date" class="opacity-50 italic text-xl">{{ date }}</p>
-    <slot />
+    <Accordion :initial-visibility="initiallyExpanded">
+      <template #title>
+        <div class="text-2xl font-semibold">
+          <img
+            v-if="iconUrl"
+            :src="iconUrl"
+            width="32"
+            height="32"
+            :alt="`${title} logo`"
+            class="icon-inline mr-2 rounded-lg"
+          />{{ title }}
+          <p v-if="date" class="opacity-50 italic text-lg font-medium">
+            {{ date }}
+          </p>
+        </div>
+      </template>
+      <template #default>
+        <slot />
+      </template>
+    </Accordion>
   </article>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import OpenInNew from "@/components/icons/OpenInNew.vue";
+import Accordion from "./Accordion.vue";
 
 export default defineComponent({
   name: "ProjectItem",
-  components: { OpenInNew },
+  components: { Accordion },
   props: {
     iconUrl: {
-      type: String,
-      default: "",
-    },
-    url: {
       type: String,
       default: "",
     },
@@ -50,6 +42,10 @@ export default defineComponent({
     date: {
       type: String,
       default: "",
+    },
+    initiallyExpanded: {
+      type: Boolean,
+      default: false,
     },
   },
 });
